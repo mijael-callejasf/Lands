8ea4ab60c47fbb3357f7279817620ebb8ca566c0
Mijael Callejas <mijael.callejasf@gmail.com>
2018-07-23 13:20:59 -0400

    Login Page, Diseño de interfaz grafica para Loguearse.

--- Lands/Lands/App.xaml.cs
+++ Lands/Lands/App.xaml.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
-namespace Lands
+﻿namespace Lands
 {
-	public partial class App : Application
+    using Views;
+    using Xamarin.Forms;
+    public partial class App : Application
 	{
-		public App ()
+        #region Constructors
+        public App ()
 		{
 			InitializeComponent();
 
-			MainPage = new Lands.MainPage();
+			this.MainPage = new NavigationPage(new LoginPage());
 		}
+        #endregion
 
-		protected override void OnStart ()
+        #region Methods
+        protected override void OnStart ()
 		{
 			// Handle when your app starts
 		}
@@ -30,5 +28,6 @@ namespace Lands
 		{
 			// Handle when your app resumes
 		}
-	}
+        #endregion
+    }
 }
